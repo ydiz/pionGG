bb27193017d6e41588a1cd2f5fc4fdbdd89cc720
Yidi Zhao <zyd2117@gmail.com>
2019-05-09 10:47:49 -0400

    May 09

--- amplitude.cc
+++ amplitude.cc
@@ -3,45 +3,96 @@
 #include "constants_macro.h"
 #include "io.h"
 #include "pGG.h"
+#include "read_leptonic.h"
+
+namespace Grid{
+namespace QCD{
+
+
+bool isWithinRegion(const std::vector<int> gcoor, int space_cutoff, int time_cutoff, const std::vector<int> fdims) {
+  if((gcoor[0] <= space_cutoff || gcoor[0] >= fdims[0] - space_cutoff) && (gcoor[1] <= space_cutoff || gcoor[1] >= fdims[1] - space_cutoff) && (gcoor[2] <= space_cutoff || gcoor[2] >= fdims[2] - space_cutoff) && (gcoor[3] <= time_cutoff || gcoor[3] >= fdims[3] - time_cutoff)) return true;
+  else return false;
+}
+
+
+double mult_hadronic_leptonic(const LatticePGG &hadronic, const LatticePGG &leptonic, int space_cutoff, int time_cutoff) {
+	LatticeComplex tmp(hadronic._grid);
+  tmp = 0.;
+	// parallel_for(int ss=0; ss<hadronic._grid->oSites(); ++ss){
+	// 	tmp[ss]()()() = hadronic[ss]()()(0, 1) * leptonic[ss]()()(0, 1) + hadronic[ss]()()(1, 0) * leptonic[ss]()()(1, 0); 
+	// 	tmp[ss]()()() += hadronic[ss]()()(0, 2) * leptonic[ss]()()(0, 2) + hadronic[ss]()()(2, 0) * leptonic[ss]()()(2, 0); 
+	// 	tmp[ss]()()() += hadronic[ss]()()(2, 1) * leptonic[ss]()()(2, 1) + hadronic[ss]()()(1, 2) * leptonic[ss]()()(1, 2); 
+	// }
+  parallel_for(int ss=0; ss<tmp._grid->lSites(); ss++){
+    std::vector<int> lcoor, gcoor;
+    localIndexToLocalGlobalCoor(tmp._grid, ss, lcoor, gcoor);
+
+    if(!isWithinRegion(gcoor, space_cutoff, time_cutoff, tmp._grid->_fdimensions)) continue; // set cutoff
+
+    typename LatticeComplex::vector_object::scalar_object tmp_site; //  this has to be defined within parallel_for
+		typename LatticePGG::vector_object::scalar_object hadronic_site, leptonic_site;
+		peekLocalSite(hadronic_site, hadronic, lcoor);
+		peekLocalSite(leptonic_site, leptonic, lcoor);
+
+		tmp_site()()() = hadronic_site()()(0, 1) * leptonic_site()()(0, 1) + hadronic_site()()(1, 0) * leptonic_site()()(1, 0); 
+		tmp_site()()() += hadronic_site()()(0, 2) * leptonic_site()()(0, 2) + hadronic_site()()(2, 0) * leptonic_site()()(2, 0); 
+		tmp_site()()() += hadronic_site()()(2, 1) * leptonic_site()()(2, 1) + hadronic_site()()(1, 2) * leptonic_site()()(1, 2); 
+    pokeLocalSite(tmp_site, tmp, lcoor);
+  }
+
+
+	Complex ret = TensorRemove(sum(tmp));
+	return ret.real();
+}
+
+
+double calculate_decay_rate(const LatticePGG &three_point, const LatticePGG &leptonic, int space_cutoff, int time_cutoff) {
+	// std::string hadronic_file = "/home/ydzhao/cuth/three_point_config/32D_ama/matrix_element";
+	// std::string leptonic_file = "/home/ydzhao/cuth/pionGG/lat_leptonic";
+  
+	// std::string hadronic_file = "./lat_config/matrix_element";
+	// readScidac(hadronic, hadronic_file);
+
+	// std::string leptonic_file = "./lat_config/lat_leptonic";
+	// readScidac(leptonic, leptonic_file);
+
+	LatticePGG hadronic(three_point._grid);
+	LatticeComplex pp(three_point._grid); 
+	get_pp(pp, "my_wall_wall.txt");
+	hadronic = three_point * pp;
+  hadronic = imag(hadronic); 
+
+  std::cout << std::string(20, '*') << std::endl;
+  std::cout << "space cutoff: " << space_cutoff << "; time cutoff: " << time_cutoff << std::endl;
+
+	double ret = mult_hadronic_leptonic(hadronic, leptonic, space_cutoff, time_cutoff);
+	std::cout << "Multiplication of hadronic and leptonic part: " << ret << std::endl;
+
+	double me = 511000;
+  double hadron_coeff = std::sqrt(std::pow(2*M_PI, 3) * 2 * M_PION) / 17853.18; // 17853.18 is <pi | pi(0) | 0>   ; normalization factor for pion operator
+	double amplitude_M = hadron_coeff * 1./ (3 * std::sqrt(2))  / (2 * M_PI) / 137. / 137. * me * ret; // * std::sqrt(std::pow(2*M_PI, 3) * 2 * M_PION); 
+	std::cout << "Amplitude(for one polarization)  M = " << amplitude_M << "eV" <<  std::endl;
+
+	double Mpi = 135000000;
+	double beta = std::sqrt(1 - 4*me*me / (Mpi*Mpi));
+	double Gamma = 2.0 * beta / (16 * M_PI * Mpi) * amplitude_M * amplitude_M; // the first factor 2.0 comes from adding two possible polarizations
+	std::cout << "Decay rate Gamma = " << Gamma << "eV" << std::endl;
+
+	double Gamma_photons = 7.75;
+	double R_real = Gamma / Gamma_photons;
+	std::cout << "Real part of branching ratio = " << R_real << "(should be 2.12e-8 or 2.82e-8)" << std::endl; 
+
+	double R_imag = 4.75e-8;
+	std::cout << "Total branching ratio = " << R_imag + R_real << "(should be 6.87e-8 or 7.57e-8)" << std::endl;
+}
+
+
+}}
 
 using namespace std;
 using namespace Grid;
 using namespace Grid::QCD;
 
-namespace Grid{
-namespace QCD{
-
-double mult_hadronic_leptonic(const LatticePGG &hadronic, const LatticePGG &leptonic) {
-	LatticeComplex tmp(hadronic._grid);
-	parallel_for(int ss=0; ss<hadronic._grid->oSites(); ++ss){
-		// tmp[ss]()()() = 2. * ( hadronic[ss]()()(0, 1) * leptonic[ss]()()(0, 1) + hadronic[ss]()()(2, 2) * leptonic[ss]()()(2, 3) ) ; 
-		// tmp[ss]()()() = 2. * hadronic[ss]()()(0, 1) * leptonic[ss]()()(0, 1); 
-		tmp[ss]()()() = hadronic[ss]()()(0, 1) * leptonic[ss]()()(0, 1) + hadronic[ss]()()(1, 0) * leptonic[ss]()()(1, 0); 
-	}
-
-  tmp = abs(tmp);
-
-	Complex ret = TensorRemove(sum(tmp));
-	return ret.real();
-}
-
-
-double mult_hadronic_leptonic_print(const LatticePGG &hadronic, const LatticePGG &leptonic) {
-	LatticeComplex tmp(hadronic._grid);
-	parallel_for(int ss=0; ss<hadronic._grid->oSites(); ++ss){
-		// tmp[ss]()()() = 2. * ( hadronic[ss]()()(0, 1) * leptonic[ss]()()(0, 1) + hadronic[ss]()()(2, 2) * leptonic[ss]()()(2, 3) ) ; 
-		// tmp[ss]()()() = 2. * hadronic[ss]()()(0, 1) * leptonic[ss]()()(0, 1); 
-		tmp[ss]()()() = hadronic[ss]()()(0, 1) * leptonic[ss]()()(0, 1) + hadronic[ss]()()(1, 0) * leptonic[ss]()()(1, 0); 
-	}
-  
-  tmp = abs(tmp);
-  std::cout<< tmp << std::endl;
-
-	Complex ret = TensorRemove(sum(tmp));
-	return ret.real();
-}
-
-}}
 
 const std::vector<int> gcoor({32, 32, 32, 64});
 
@@ -51,40 +102,21 @@ int main(int argc, char* argv[])
   Grid_init(&argc, &argv);
 
   GridCartesian * grid = SpaceTimeGrid::makeFourDimGrid(gcoor, GridDefaultSimd(Nd,vComplex::Nsimd()), GridDefaultMpi());
-	LatticePGG hadronic(grid);
+
+  int space_cutoff = 16;
+  int time_cutoff = 16;
+
 	LatticePGG leptonic(grid);
 
-	// std::string hadronic_file = "/home/ydzhao/cuth/three_point_config/32D_ama/matrix_element";
-	// std::string leptonic_file = "/home/ydzhao/cuth/pionGG/lat_leptonic";
-	std::string hadronic_file = "./lat_config/matrix_element";
-	std::string leptonic_file = "./lat_config/lat_leptonic";
+	LatticePGG avg(grid);
+	readScidac(avg, "./lat_config/average_three_point_sloppy");
 
-	readScidac(hadronic, hadronic_file);
-	// hadronic = timesI(hadronic); //this is done before writing matrix_elment // cheng's three piont function is imaginary
-	// cout << hadronic << endl;
-  //
-	readScidac(leptonic, leptonic_file);
+	std::string filename_p3 = "/home/ydzhao/cuth/cuba_integration/results/p30e10-5Cuhre_with_p3/data.txt";
+	std::string filename_p1 = "/home/ydzhao/cuth/cuba_integration/results/p30e10-4Cuhre_with_p1/data.txt";
+	get_leptonic(filename_p1, filename_p3, leptonic, SPACE_LIMIT, TIME_LIMIT);
 
-	double ret = mult_hadronic_leptonic(hadronic, leptonic);
-	cout << "Multiplication of hadronic and leptonic part: " << ret << endl;
+  calculate_decay_rate(avg, leptonic, space_cutoff, time_cutoff); 
 
-	double me = 511000;
-	double amplitude_M = 1./ (3 * std::sqrt(2))  / (2 * M_PI) / 137. / 137. * me * ret; 
-	cout << "Amplitude(for one polarization)  M = " << amplitude_M << "eV" <<  endl;
-
-	double Mpi = 135000000;
-	double beta = std::sqrt(1 - 4*me*me / (Mpi*Mpi));
-	double Gamma = 2.0 * beta / (16 * M_PI * Mpi) * amplitude_M * amplitude_M; // the first factor 2.0 comes from adding two possible polarizations
-	cout << "Decay rate Gamma = " << Gamma << "eV" << endl;
-
-	double Gamma_photons = 7.75;
-	double R_real = Gamma / Gamma_photons;
-	cout << "Real part of branching ratio = " << R_real << "(should be 2.12e-8 or 2.82e-8)" << endl; 
-
-	double R_imag = 4.75e-8;
-	cout << "Total branching ratio = " << R_imag + R_real << "(should be 6.87e-8 or 7.57e-8)" << endl;
-
-	mult_hadronic_leptonic_print(hadronic, leptonic);
 
 	Grid_finalize();
   return 0;
--- pGG.h
+++ pGG.h
@@ -3,11 +3,13 @@
 #include <qlat/qlat.h>
 #include "io.h"
 #include "constants_macro.h"
+#include "utils.h"
+
 
 namespace Grid {
 namespace QCD {
 
-// to use Scidac, we make PGGElem has only one level (like iMatrix<Complex, 4>)
+// to use Scidac, we cannot make PGGElem have only one level (like iMatrix<Complex, 4>)
 using PGGElem = iScalar<iScalar<iMatrix<Complex, 4>>>;
 using vPGGElem = iScalar<iScalar<iMatrix<vComplex, 4>>>;
 using LatticePGG = Lattice<vPGGElem>;
@@ -21,55 +23,18 @@ void print_grid_field_site(const T &field, const std::vector<int> coor) {
 	std::cout << site << std::endl;
 }
 
-// get  exp(- 2 Mpi t) / sqrt( exp(Mpi t) <pi(0) pi(t)> ) for t in [0, T/2 - 10)
 void get_pp(LatticeComplex &lat, const std::string &filename, int tmin=10) {
 
-	int Tsize =  lat._grid->_fdimensions[Tdir];
-	
-	std::vector<double> pps = read_pp(filename);
-	assert(pps.size() == Tsize);
-	
 	parallel_for(int ss=0; ss<lat._grid->lSites(); ss++){
-		std::vector<int> lcoor(4);
-		lat._grid->LocalIndexToLocalCoor(ss, lcoor);
+    std::vector<int> lcoor, gcoor;
+    localIndexToLocalGlobalCoor(lat._grid, ss, lcoor, gcoor);
 
-		std::vector<int> gcoor(4);
-		std::vector<int> processor_coor;
-		lat._grid->ProcessorCoorFromRank(lat._grid->ThisRank(), processor_coor);
-		lat._grid->ProcessorCoorLocalCoorToGlobalCoor(processor_coor, lcoor, gcoor);
-		//std::cout << processor_coor << lcoor << gcoor << std::endl;
-
-    // ===================================
-    // cheng's tsep
-    std::vector<int> x(4,0);
-    std::vector<int> xp = gcoor;
-    int t_min = 10;
-    int t_wall;
-    int t_sep;
-
-    using namespace qlat;
-    int diff = qlat::smod(x[3] - xp[3], 64);
-    if (diff >= 0)
-    {
-      t_wall = qlat::mod(x[3] + t_min, 64);
-    } else {
-      t_wall = qlat::mod(xp[3] + t_min, 64);
-    }
-    t_sep = qlat::mod(t_wall - x[3], 64);
-
-    // ===================================
-
-		int xt = smod(gcoor[Tdir], Tsize);
 		double val;
-		if(xt <= TIME_LIMIT && xt >= -TIME_LIMIT) {
-			int pion_t = (xt<=0) ? tmin : xt + tmin;
-      assert(pion_t == t_wall); // FIXME: delete me
-			val = 1. / std::sqrt(std::exp( - M_PION * pion_t) * pps[pion_t]); //FIXME: do I need to change this sign of pion_t?
-			// val *= std::exp( - M_PION * pion_t); 
-			// val *= std::exp( - M_PION * t_sep); // FIXME: cheng's t_sep is strange
-		}
-		else val = 0. ;
-		
+			// val = 1. / std::sqrt(std::exp( - M_PION * pion_t) * pps[pion_t]); //FIXME: do I need to change this sign of pion_t?
+    // val = 1. 
+		int xt = qlat::smod(gcoor[Tdir], lat._grid->_fdimensions[Tdir]);
+    val = std::exp( 0.5 * M_PION * xt); // translation factor
+
 		typename LatticeComplex::vector_object::scalar_object m;
 		m()()() = Complex(val, 0.);
 		pokeLocalSite(m, lat, lcoor);
