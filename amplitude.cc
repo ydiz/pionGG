449c7e8e0d63ec5124c67b57afb1263444c94e17
ydzhao <zyd2117@gmail.com>
2019-05-15 00:17:45 +0000

    xxx

--- amplitude.cc
+++ amplitude.cc
@@ -110,10 +110,13 @@ int main(int argc, char* argv[])
 	LatticePGG leptonic(grid);
 
 	LatticePGG avg(grid);
-	readScidac(avg, "./lat_config/average_three_point_sloppy");
+	// readScidac(avg, "./lat_config/average_three_point_sloppy");
+	readScidac(avg, "./lat_config/hadronic_wall_on_right/average_three_point_sloppy");
 
-	std::string filename_p3 = "/home/ydzhao/cuth/cuba_integration/results/p30e10-5Cuhre_with_p3/data.txt";
-	std::string filename_p1 = "/home/ydzhao/cuth/cuba_integration/results/p30e10-4Cuhre_with_p1/data.txt";
+	// std::string filename_p3 = "/home/ydzhao/cuth/cuba_integration/results/p30e10-5Cuhre_with_p3/data.txt";
+	// std::string filename_p1 = "/home/ydzhao/cuth/cuba_integration/results/p30e10-4Cuhre_with_p1/data.txt";
+  std::string filename_p3 = "/projects/HadronicLight_4/yidizhao/cooley/pionGG/integrals/p30e10-5Cuhre_with_p3/data.txt";
+  std::string filename_p1 = "/projects/HadronicLight_4/yidizhao/cooley/pionGG/integrals/p30e10-4Cuhre_with_p1/data.txt";
 	get_leptonic(filename_p1, filename_p3, leptonic, SPACE_LIMIT, TIME_LIMIT);
 
   calculate_decay_rate(avg, leptonic, space_cutoff, time_cutoff); 
