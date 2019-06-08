ccd6089802e650143b6b75bf8809cd62ff18e125
Yidi Zhao <zyd2117@gmail.com>
2019-06-07 21:07:34 -0400

    Cleaned up code for calculating integral

--- integral/int.cc
+++ integral/int.cc
@@ -8,14 +8,43 @@
 
 #include "cuba_wrapper.h"
 
-// For different ensembles, only need to change M_PION
-// #define M_PION 0.139474
-#define M_PION 0.10025
+// // For different ensembles, only need to change M_PION, L_LIMIT and target
+// w_x from -L_LIMIT to +L_LIMIT
+// w_0 from -T_LIMIT to +T_LIMIT
+// target determines the value of beta
 
-constexpr double me = 511000;
-constexpr double Mpi = 135000000;
-constexpr double beta = std::sqrt(1 - 4*me*me / (Mpi*Mpi));
+// ================= physical parameters ========================
 
+// #define M_PION 0.139474 // 32ID
+// #define M_PION 0.10468 // 32IDF
+#define M_PION 0.13975 // 24ID
+
+#define L_LIMIT 12
+// #define L_LIMIT 16 
+
+#define T_LIMIT 16
+
+const std::string target("Pion");
+
+// ================ integration parameters ===================
+const double upper = 50; // = 30;
+const double epsrel = 1e-6; // = 1e-4;
+
+
+
+//================= code ===============================
+
+double get_beta(const std::string &tar) {
+  const double me = 0.511;
+  const double Mpi = 135;
+  const double m_mu = 105.658;
+  const double M_KL = 497.611;
+  if(tar=="Pion") return std::sqrt(1 - 4*me*me / (Mpi*Mpi));
+  else if(tar=="Kaon") return std::sqrt(1 - 4*m_mu*m_mu / (M_KL*M_KL));
+  else assert(0);
+}
+
+// p.s. integrate_qawc will add a term (p - pole) to the denominator; DO NOT write this factor in the function
 double f1(double p, void *params) {
   std::vector<double> paras = *(std::vector<double> *)params;
   double w = paras[0];
@@ -23,11 +52,9 @@ double f1(double p, void *params) {
   double sin_pw, cos_pw;
   sincos(p * w, &sin_pw, &cos_pw);
 
-  // return std::exp(-p * w0) / (M_PION - 2 * p) * (cos_pw - sin_pw / (p * w));
   return - 0.5 * std::exp(-p * w0) * (cos_pw - sin_pw / (p * w)); // f should not contain the pole term if using qaws
 }
 
-// p.s. integrate_qawc will add a term (p - pole) to the denominator; DO NOT write this factor in the function
 double f2(double p, void *params) {
   std::vector<double> paras = *(std::vector<double> *)params;
   double w = paras[0];
@@ -61,11 +88,17 @@ void integrate_qawc(T f, std::vector<double> params, double lower, double upper,
 struct Func {
 
   double p_interval;
-	double pe = 0.5 * M_PION * beta;
+	double pe; // magnitude of electron's momentum. 
   std::vector<double> paras; // [w, w0]
 
+  Func(double beta);
   std::vector<double> operator()(const std::vector<double>& v) const;
 };
+
+
+Func::Func(double beta) {
+  pe = 0.5 * M_PION * beta;
+}
 
 std::vector<double> Func::operator()(const std::vector<double>& v) const{
 
@@ -96,7 +129,6 @@ double integrate_CUBA(T func, double eps_rel)
 
 	integrateCuhre(integral, error, prob, nregions, neval, fail, 3, 1, func, 0., eps_rel);
   return integral[0];
-
 }
 
 
@@ -104,21 +136,33 @@ gsl_integration_workspace * workspace = gsl_integration_workspace_alloc (1000);
 
 int main (void)
 {
+  int w_max = int(L_LIMIT * std::sqrt(3)) + 1;
+  int w0_max = T_LIMIT;
+  double beta = get_beta(target);
   std::cout << "M_H (in lattice unit): " << M_PION << std::endl;
+  std::cout << "w: [" << -w_max << ", " << w_max << "]" << std::endl;
+  std::cout << "w0: [" << -w0_max << ", " << w0_max << "]" << std::endl;
+  std::cout << "beta: " << beta << std::endl;
+  std::cout << std::string(30, '*') << std::endl;
+  std::cout << "Upper limit of integral: " << upper << std::endl;
+  std::cout << "Allowed relative error: " << epsrel << std::endl;
   std::cout << std::string(30, '*') << std::endl;
   
   double ret1, ret2, ret3, error;
-  double lower = 0.00001, upper = 30, epsrel = 1e-4;
+  // double lower = 0.00001, upper = 30, epsrel = 1e-4;
+  // double lower = 0.00001, upper = 30, epsrel = 1e-6;
+  // double lower = 0.00001, upper = 50, epsrel = 1e-6;
+  double lower = 0.00001;
 
   double log_beta = std::log((1 + beta) / (1 - beta));
 
-  Func f3;
+  Func f3(beta);
   f3.p_interval = upper;
 
-  for(double w=0; w<=28; w+=1.) 
-    for(int w0=0; w0<=16; ++w0) {
+  for(int w=0; w<=w_max; ++w) 
+    for(int w0=0; w0<=w0_max; ++w0) {
       double ret;
-      std::vector<double> params {w, double(w0)};
+      std::vector<double> params {double(w), double(w0)};
       std::cout << "w=" << w << " w0=" << w0 << std::endl;
 
       f3.paras = params;
@@ -134,10 +178,9 @@ int main (void)
               + 2.0 * M_PI / w / w * ret3;
       }
       std::cout << std::setprecision(10) << "integral = " << ret  << std::endl;
-
   }
 
-  gsl_integration_workspace_free (workspace);
+  gsl_integration_workspace_free(workspace);
 
   return 0;
 }
